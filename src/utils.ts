c4d85d486b244dd3338d2cf6691eadae3c12e93a
Sébastien Dan <sebastien.dan@gmail.com>
2023-09-04 19:43:01 +0200

    fix: check if ip address when sanitizing urls

--- src/utils.ts
+++ src/utils.ts
@@ -8,7 +8,20 @@ export function shortenAddress(
 }
 
 export function sanitizeURLProtocol(protocol: 'ws' | 'http', endpoint: string) {
-  return location.protocol.startsWith('https')
+  return location.protocol.startsWith('https') &&
+    endpoint.indexOf('localhost') === -1 &&
+    !isStringIpAddress(endpoint)
     ? `${protocol}s://${endpoint}`
     : `${protocol}://${endpoint}`
+}
+
+function isStringIpAddress(value: string) {
+  if (
+    /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(
+      value
+    )
+  ) {
+    return true
+  }
+  return false
 }
